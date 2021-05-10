bbcde48260ed34e62ef14b58ac98643e32bc25d5
Luís Fernando <luisfernando.25@hotmail.com>
2021-05-09 23:34:00 -0300

    Criar metodo de atualizar pessoa Atualizar README

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Person API
 
-Projeto feito para o bootcamp Code Anywhere da Digital Innovation One.
+O Projeto consiste em uma API REST com Spring Boot feito para o bootcamp Code Anywhere da Digital Innovation One.
+Para o desenvolvimento da API foi foi utilizado o [Spring initializr](https://start.spring.io/) para criar o projeto com 
+suas dependências, a versão 11 do Java, Gradle e a IDE Intellj IDEA Community Edition.
+[Link do projeto no heroku.](https://personapi-live-dio.herokuapp.com/)
--- src/main/java/com/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/com/digitalinnovation/personapi/service/PersonService.java
@@ -6,6 +6,7 @@ import com.digitalinnovation.personapi.entity.Person;
 import com.digitalinnovation.personapi.exception.PersonNotFoundException;
 import com.digitalinnovation.personapi.mapper.PersonMapper;
 import com.digitalinnovation.personapi.repository.PersonRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,25 +14,21 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService {
 
     private PersonRepository personRepository;
 
     private final PersonMapper personMapper = PersonMapper.INSTANCE;
 
-    @Autowired
-    public PersonService(PersonRepository personRepository){
-        this.personRepository = personRepository;
-    }
-
     public MessageResponseDTO createPerson(PersonDTO personDTO){
         Person personToSave = personMapper.toModel(personDTO);
 
         Person savedPerson = personRepository.save(personToSave);
-        return MessageResponseDTO
-                .builder()
-                .message("Created person with ID " + savedPerson.getId())
-                .build();
+
+        MessageResponseDTO message = createMessageResponse(savedPerson.getId(), "Created person with ID ");
+
+        return message;
     }
 
     public List<PersonDTO> listAll() {
@@ -50,7 +47,25 @@ public class PersonService {
         personRepository.deleteById(id);
     }
 
+    public MessageResponseDTO updateByID(Long id, PersonDTO personDTO) throws PersonNotFoundException {
+        verifyIfExits(id);
+
+        Person personToUpdate = personMapper.toModel(personDTO);
+        Person updatedPerson = personRepository.save(personToUpdate);
+
+        MessageResponseDTO message = createMessageResponse(updatedPerson.getId(), "Updated person with ID ");
+
+        return message;
+    }
+
     private Person verifyIfExits(Long id) throws PersonNotFoundException {
         return personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException(id));
+    }
+
+    private MessageResponseDTO createMessageResponse(Long id, String message){
+        return MessageResponseDTO
+                .builder()
+                .message(message + id)
+                .build();
     }
 }
